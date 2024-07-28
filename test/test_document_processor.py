8ab9352c8615c928381cfcd4ade2a466f465fad6
ornot-4090 <ornot2009@gmail.com>
2024-07-28 06:20:33 +0000

    text_docement_processor passed unit test

--- test/test_document_processor.py
+++ test/test_document_processor.py
@@ -1,10 +1,16 @@
 import unittest
 
 from config import gpt2_cfg
+from document_processor import TextDocumentProcessor
 
 class TestDocumentProcessor(unittest.TestCase):
     def test_document_processor(self):
-        pass
+        doc_processor = TextDocumentProcessor()
+
+        raw_text = doc_processor(gpt2_cfg.dataset["xjtu_1"]['path'])
+        print(raw_text[:100])
+   
+
     
 
 if __name__ == "__main__":
