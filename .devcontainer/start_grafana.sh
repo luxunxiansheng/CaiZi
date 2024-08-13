613d153dcc8d59d7fe331eb62df4d524f79d6396
bin.li <bin.li@raysightmed.com>
2024-08-13 13:31:16 +0000

    now can start promethues in backgrond with screen

--- .devcontainer/start_grafana.sh
+++ .devcontainer/start_grafana.sh
@@ -1,1 +1,3 @@
- /usr/sbin/grafana-server --config=/tmp/ray/session_latest/metrics/grafana/grafana.ini --homepath=/usr/share/grafana web
+#!/bin/bash
+ 
+/usr/sbin/grafana-server --config=/tmp/ray/session_latest/metrics/grafana/grafana.ini --homepath=/usr/share/grafana web
--- .devcontainer/start_prometheus.sh
+++ .devcontainer/start_prometheus.sh
@@ -1,1 +1,7 @@
-screen -r -d -m prometheus --config.file=prometheus.yml
+#!/bin/bash
+
+# Start a new detached screen session and run the Prometheus script
+screen -dmS prometheus-session bash -c "prometheus --config.file=prometheus.yml"
+
+# Optional: Display a message
+echo "Prometheus has been started in a detached screen session named 'prometheus-session."
