2fa3733bd670d5c34b8eb8a6c65e77d6a98b77f3
ornot-4090 <ornot2009@gmail.com>
2024-09-09 06:24:47 +0000

    poststartcommand to start ray, prom,grafana

--- .devcontainer/start_prometheus.sh
+++ .devcontainer/start_prometheus.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start a new detached screen session and run the Prometheus script
-screen -dmS prometheus-session bash -c "prometheus --config.file=prometheus.yml"
+screen -dmS prometheus-session bash -c "cd .devcontainer && prometheus --config.file=prometheus.yml"
 
 # Optional: Display a message
 echo "Prometheus has been started in a detached screen session named 'prometheus-session."
